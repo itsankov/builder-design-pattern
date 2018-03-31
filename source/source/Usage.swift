f8bb9d5fe4742f7be08a0dec282a6d6675d9ce41
Ivelin Tsankov <ivelin.tsankov@emerchantpay.com>
2018-03-31 17:34:19 +0300

    update Usage

--- source/source/Usage.swift
+++ source/source/Usage.swift
@@ -6,6 +6,14 @@ var director = ConnectionDirector(connectionBuilder: firstBuilder)
 director.constructConnection()
 var connection = director.connection
 
+print(connection.urlString)//firsturl.com
+print(connection.username)//firstUsername
+print(connection.password)//firstPassword
+
 director = ConnectionDirector(connectionBuilder: secondBuilder)
 director.constructConnection()
 connection = director.connection
+
+print(connection.urlString)//secondurl.com
+print(connection.username)//secondUsername
+print(connection.password)//secondPassword
